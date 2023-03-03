977fddce43918584591bc15ab70eabbd3e3385a0
shreeyasatam <shreeyasatam@yahoo.co.in>
2023-03-03 13:02:36 +1100

    first commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+
+    return (
+        <div className="note"> 
+        
+            <h1>This is a Title </h1>
+            <p> This is  content </p>
+        
+        </div>
+
+    );
+}
+
+export default Note;
